2487fd92d92492764d2a0620598cdd0966dd1643
MrTrick <MrTrick@github>
2014-09-03 17:28:05 +1000

    Improvements - added support for value and required/enable/disable bindings

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ A simple knockout binding that integrates with knockout's select support.
  - Passing select options through the 'options' binding.
  - Passing selectize parameters through the 'selectize' binding.
  - Transparent bi-directional management of options, if 'options' is bound to an observable.
+ - Transparent bi-directional management of value, if 'value' is bound to an observable.
+ - Supports 'required' and 'enabled'/'disabled' bindings, detects element mutation.
 
 Issues, feature requests etc welcome.
--- knockout-selectize.js
+++ knockout-selectize.js
@@ -3,22 +3,23 @@
  * Version: 0.1
  * License: MIT
  * Author: MrTrick
+ * https://github.com/MrTrick/knockout-selectize
  */
 
 ko.bindingHandlers.selectize = {
-    init: function (el, value, bindings, vm, context) {
-        var value = value();
-        var params = _.defaults( 
-            ko.unwrap(value), 
-            {valueField:'value', labelField:'text'}
-        );
+    init: function (el, bindingValue, bindings, vm, context) {
+        var bindingValue = bindingValue();
+        var params = ko.unwrap(bindingValue);
+        params.valueField || (params.valueField = 'value');
+        params.labelField || (params.labelField = 'text');
         
-        $(el).selectize(params);
+        var $el = $(el);
+        if (!$el.is('select')) throw "The selectize knockout binding is only valid on <select> elements. (because of the options binding)";
+        $el.selectize(params);
         var selectize = el.selectize;
         
         //What options are being given?
         var options = bindings.get('options');
-        
         //If the options are observable, synchronise add/remove events between selectize and knockout
         if (ko.isObservable(options)) {
             var changing = false;
@@ -41,7 +42,50 @@ ko.bindingHandlers.selectize = {
             }, null, 'arrayChange');
         }
         
-        //When the dom node is removed, ensure that the selectize node is too.
-        ko.utils.domNodeDisposal.addDisposeCallback(el, function() { el.selectize.destroy(); });
+        //What is the input's value? 
+        var value = bindings.get('value');
+        //If the value is observable, synchronise 
+        if (ko.isObservable(value)) {
+            value.subscribe(function(value) {
+               //If the observable value doesn't match the element value, update the element;
+               if (value !== selectize.getValue()) {
+                   if (typeof selectize.loadOption === "function") { selectize.loadOption(value); } 
+                   else { selectize.setValue(value); }
+               }
+            });
+        }
+        
+        //For knockout bindings to work, we need to observe when the attributes of the <select> element are changed - `required`, `disabled`.
+        //This is to update the selectize control to match the <select> element
+        //(NOTE: `visible` should not be bound directly to <select>. Bind `visible` to an enclosing <div> or similar.) 
+        var observer = new MutationObserver(function(mutations) {
+            var disabled = selectize.$input.is(':disabled'),
+                required = selectize.$input.is(':required');
+
+            //Check disabled
+            //(The mutation may fire multiple times, so fence against loops)
+            if (disabled !== selectize.isDisabled) {
+                //Disable/enable selectize to match the real select element
+                selectize[disabled ? 'disable' : 'enable']();
+                
+                //When the real element is disabled, disable the control input too.
+                //This is a selectize bug - #307
+                selectize.$control_input.prop('disabled', disabled);
+            }
+            
+            //Check required
+            //(The mutation may fire multiple times, so fence against loops)
+            if (required !== selectize.isRequired) {
+                selectize.isRequired = required;
+                selectize.refreshState();
+            }
+        });
+        observer.observe(el, {attributes:true});
+        
+        //When the dom node is removed, ensure that the selectize node is too, as well as the mutation observer
+        ko.utils.domNodeDisposal.addDisposeCallback(el, function() { 
+            selectize.destroy(); 
+            observer.disconnect();
+        });
     }    
 };
