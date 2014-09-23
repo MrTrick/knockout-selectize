4c6a7dfcbc313b5d116d673872cdd5c0be961eec
David Díaz <ddiaz@citec-sl.com>
2014-09-23 08:06:17 +0100

    Removes trailing spaces

--- knockout-selectize.js
+++ knockout-selectize.js
@@ -12,26 +12,26 @@ ko.bindingHandlers.selectize = {
         var params = ko.unwrap(bindingValue);
         params.valueField || (params.valueField = 'value');
         params.labelField || (params.labelField = 'text');
-        
+
         var $el = $(el);
         if (!$el.is('select')) throw "The selectize knockout binding is only valid on <select> elements. (because of the options binding)";
         $el.selectize(params);
         var selectize = el.selectize;
-        
+
         //What options are being given?
         var options = bindings.get('options');
         //If the options are observable, synchronise add/remove events between selectize and knockout
         if (ko.isObservable(options)) {
             var changing = false;
-            
-            //Whenever an option is added/removed, copy to the observable. 
+
+            //Whenever an option is added/removed, copy to the observable.
             selectize.on('option_add', function(value, data) { if (!changing) options.push(value); } );
             selectize.on('option_remove', function(value) { if (!changing) options.remove(value); } );
-            
-            //Whenever the observable has an element added/removed, copy to the options 
+
+            //Whenever the observable has an element added/removed, copy to the options
             options.subscribe(function(changes) {
                 changing = true;
-                changes.forEach(function(change) { 
+                changes.forEach(function(change) {
                     if (change.status === 'added') {
                         option = {};
                         option[params.labelField] = change.value;
@@ -44,26 +44,26 @@ ko.bindingHandlers.selectize = {
                 changing = false;
             }, null, 'arrayChange');
         }
-        
-        //What is the input's value? 
+
+        //What is the input's value?
         var value = bindings.get('value');
-        //If the value is observable, synchronise 
+        //If the value is observable, synchronise
         if (ko.isObservable(value)) {
             value.subscribe(function(value) {
                //If the observable value doesn't match the element value, update the element;
                if (value !== selectize.getValue()) {
-                   if (typeof selectize.loadOption === "function") { selectize.loadOption(value); } 
+                   if (typeof selectize.loadOption === "function") { selectize.loadOption(value); }
                    else { selectize.setValue(value); }
                }
             });
         }
 
-        //Selectize bug; doesn't disable the control input if the parent is disabled, even at startup. Fix. (after startup, handled by the observer ahead) 
+        //Selectize bug; doesn't disable the control input if the parent is disabled, even at startup. Fix. (after startup, handled by the observer ahead)
         if (selectize.$input.is(':disabled')) selectize.$control_input.prop('disabled', true);
-        
+
         //For knockout bindings to work, we need to observe when the attributes of the <select> element are changed - `required`, `disabled`.
         //This is to update the selectize control to match the <select> element
-        //(NOTE: `visible` should not be bound directly to <select>. Bind `visible` to an enclosing <div> or similar.) 
+        //(NOTE: `visible` should not be bound directly to <select>. Bind `visible` to an enclosing <div> or similar.)
         var observer = new MutationObserver(function(mutations) {
             var disabled = selectize.$input.is(':disabled'),
                 required = selectize.$input.is(':required');
@@ -73,12 +73,12 @@ ko.bindingHandlers.selectize = {
             if (disabled !== selectize.isDisabled) {
                 //Disable/enable selectize to match the real select element
                 selectize[disabled ? 'disable' : 'enable']();
-                
+
                 //When the real element is disabled, disable the control input too.
                 //This is a selectize bug - #307
                 selectize.$control_input.prop('disabled', disabled);
             }
-            
+
             //Check required
             //(The mutation may fire multiple times, so fence against loops)
             if (required !== selectize.isRequired) {
@@ -87,10 +87,10 @@ ko.bindingHandlers.selectize = {
             }
         });
         observer.observe(el, {attributes:true});
-        
+
         //When the dom node is removed, ensure that the selectize node is too, as well as the mutation observer
-        ko.utils.domNodeDisposal.addDisposeCallback(el, function() { 
-            selectize.destroy(); 
+        ko.utils.domNodeDisposal.addDisposeCallback(el, function() {
+            selectize.destroy();
             observer.disconnect();
         });
     }    
